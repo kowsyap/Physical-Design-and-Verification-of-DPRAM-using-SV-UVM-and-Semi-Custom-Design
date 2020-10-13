2a212f5904a506fc7cbf2015db20594fe7998176
kowsyap pranay <pranaykowsyap@gmail.com>
2020-10-13 18:33:18 +0530

    Delete SEQR2.sv

--- UVM_ENVIRONMENT/SEQR2.sv
+++
@@ -1,9 +0,0 @@
-class dram_seqr2 extends uvm_sequencer#(dram_seq_item);
-
-`uvm_component_utils(dram_seqr2)
-
-function new(string name="dram_seqr2",uvm_component parent);
-super.new(name,parent);
-endfunction
-
-endclass
